7f1943b772c554a06f7836a85c9e4c28af5022b0
Rozag <rozag26@mail.ru>
2014-10-25 13:33:50 +0400

    Second commit

--- src/com/vsrstudio/Papaya/activities/MainActivity.java
+++ src/com/vsrstudio/Papaya/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.vsrstudio.Papaya;
+package com.vsrstudio.Papaya.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.vsrstudio.Papaya.R;
 
 public class MainActivity extends Activity {
     /**
@@ -10,6 +11,6 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_activity);
     }
 }
