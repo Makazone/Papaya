ff627271c993f64f3e39615eff356e968275d44d
Rozag <rozag26@mail.ru>
2014-10-25 14:06:05 +0400

    fix2

--- src/com/vsrstudio/papaya/activities/MainActivity.java
+++ src/com/vsrstudio/papaya/activities/MainActivity.java
@@ -2,13 +2,10 @@ package com.vsrstudio.papaya.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.vsrstudio.papaya.R;
+import com.vsrstudio.Papaya.R;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
