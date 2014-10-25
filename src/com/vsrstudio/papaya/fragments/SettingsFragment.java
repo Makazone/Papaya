77eaab6637e2ad262b29b611d180df6d6e3a66c5
Rozag <rozag26@mail.ru>
2014-10-25 20:52:13 +0400

    Рега/логин работают

--- src/com/vsrstudio/papaya/fragments/SettingsFragment.java
+++ src/com/vsrstudio/papaya/fragments/SettingsFragment.java
@@ -2,13 +2,21 @@ package com.vsrstudio.papaya.fragments;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import com.parse.DeleteCallback;
+import com.parse.ParseException;
+import com.vsrstudio.papaya.Papaya;
 import com.vsrstudio.papaya.R;
+import com.vsrstudio.papaya.activities.LoginActivity;
+import com.vsrstudio.papaya.model.User;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     private Context context;
 
@@ -17,9 +25,44 @@ public class SettingsFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-
         context = rootView.getContext();
+
+        final Button logoutButton = (Button) rootView.findViewById(R.id.logout_button);
+        logoutButton.setTypeface(Papaya.robotoLight);
+        logoutButton.setOnClickListener(this);
+
+        final Button deleteUserButton = (Button) rootView.findViewById(R.id.delete_user_button);
+        deleteUserButton.setTypeface(Papaya.robotoLight);
+        deleteUserButton.setOnClickListener(this);
+
         return rootView;
     }
 
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.logout_button:
+                logoutAndExit();
+                break;
+            case R.id.delete_user_button:
+                User.getCurrentUser().deleteInBackground(new DeleteCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            logoutAndExit();
+                        } else {
+                            e.printStackTrace();
+                            Toast.makeText(context, R.string.connection_error, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+                break;
+        }
+    }
+
+    private void logoutAndExit() {
+        Intent intent;
+        User.logOut();
+        intent = new Intent(getActivity(), LoginActivity.class);
+        getActivity().startActivity(intent);
+        getActivity().finish();
+    }
 }
