27e8112ea068a104ec2bfc4703fb4fb6c4377754
Stetsenko Makar <makazone@gmail.com>
2014-10-25 17:53:57 +0400

    Parse config method

--- src/com/vsrstudio/papaya/Papaya.java
+++ src/com/vsrstudio/papaya/Papaya.java
@@ -3,8 +3,23 @@ package com.vsrstudio.papaya;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import com.parse.Parse;
 
 public class Papaya {
+
+    /**
+     * PARSE SET UP METHODS
+     */
+
+    private static boolean wasSetUp = false;
+    public static void setUpParse(Context context) {
+        if (!wasSetUp) {
+            Parse.initialize(context, "5jOeErzAv4j5BCWsLxNrjicpDvnhnH5cyyds6X4n", "gKJOrNRPpxW9i4lyWwaVog3apmaNsI3HR02sft4k");
+            wasSetUp = true;
+        }
+    }
+
+    /** PARSE METHODS END **/
 
     public static Typeface robotoSlabRegular;
     public static Typeface robotoSlabLight;
